0b1e9308cdafd3cd6a5935fa080827e949fec2a1
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-06 16:25:29 +0500

    Initial commit

---
+++ database/migrations/2018_05_06_111007_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('image_id')->unsigned();
+            $table->foreign('image_id')->references('id')->on('images');
+            $table->string('author');
+            $table->string('title');
+            $table->string('slug');
+            $table->integer('readtime');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
