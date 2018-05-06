05ada2a5b2f50244a0d97d2784ed2294d0cc5257
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-07 00:15:56 +0500

    Modified Article Model

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    protected $table = 'articles';
+    protected $primaryKey = 'id';
+    protected $hidden = [
+        'image_id',
+    ];
 
+    public function image()
+    {
+        return $this->hasOne(Image::class, 'id', 'image_id');
+    }
 }
